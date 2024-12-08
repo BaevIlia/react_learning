af67fe0c08ba62ad205cd8f98d566c152a9a387f
Ilia Baev <baevilia.201@yandex.ru>
2024-12-08 13:23:52 +0300

    Рефакторинг, создание кастомных хуков

--- src/App.js
+++ src/App.js
@@ -5,53 +5,43 @@ import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: 'Javascript', body: 'Desc'},
-        {id: 2, title: 'Javascript', body: 'Desc'},
-        {id: 3, title: 'Javascript', body: 'Desc'}
-    ])
+    const [posts, setPosts] = useState([])
 
-    const createPost = (newPost) =>{
+    const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModalOpen(false);
     }
 
-    const removePost = (post) =>{
+    const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
     const [filter, setFilter] = useState({sort: "", query: ""});
 
-    const sortedPosts = useMemo(()=>{
-        if(filter.sort){
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-        }
-        return posts;
-    }, [filter.sort, posts]);
 
-    const sortedAndSearchedPosts = useMemo(()=>{
-        return sortedPosts.filter(post=>post.title.toLowerCase().includes(filter.query.toLowerCase()))
-    }, [filter.query, sortedPosts])
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
 
     const [modalOpen, setModalOpen] = useState(false);
 
-  return (
-    <div className="App">
-        <MyButton style={{marginTop: 10}} onClick={() => setModalOpen(true)}>
-            Создать пост
-        </MyButton>
-        <MyModal visible={modalOpen} setVisible={setModalOpen}>
-            <PostForm create={createPost}/>
-        </MyModal>
-        <hr style={{margin: 15}}/>
-        <div>
-            <PostFilter filter={filter} setFilter={setFilter}/>
+    return (
+        <div className="App">
+            <MyButton style={{marginTop: 10}} onClick={() => setModalOpen(true)}>
+                Создать пост
+            </MyButton>
+            <MyModal visible={modalOpen} setVisible={setModalOpen}>
+                <PostForm create={createPost}/>
+            </MyModal>
+            <hr style={{margin: 15}}/>
+            <div>
+                <PostFilter filter={filter} setFilter={setFilter}/>
+            </div>
+            <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты"/>
         </div>
-        <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты"/>
-    </div>
-  );
+    );
 }
 
 export default App;
